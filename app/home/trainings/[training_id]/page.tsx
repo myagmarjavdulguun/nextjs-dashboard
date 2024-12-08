690f22bec9eff0cb3ef61e99999afefe1cb5367e
Dulguun <dulguunmyagmarjav196@gmail.com>
2024-12-08 22:11:58 +0800

    completed all i think

--- app/home/trainings/[training_id]/page.tsx
+++ app/home/trainings/[training_id]/page.tsx
@@ -1,21 +1,14 @@
 import { getParticipations, getTrainings, isLoggedIn } from "@/app/lib/data";
 import TrainingDetails from "@/app/ui/trainings-detail";
 
-// Defining the correct interface for params
-interface Params {
-  training_id: string;
-}
-
-export default async function TrainingDetailsPage({ params }: { params: Params }) {
-  const data = await isLoggedIn();
+export default async function Page({ params }: { params: { training_id: string } }) {
   const { training_id } = params;  // Destructuring training_id from params
 
-  const training = await getTrainings(training_id);
-  const participations = await getParticipations(training_id);
-
-  if (!training) {
-    return <div>Training not found</div>;
-  }
+  const data = await isLoggedIn();  // Fetch login data
+  const [training, participations] = await Promise.all([  // Fetch training and participations in parallel
+    getTrainings(training_id),
+    getParticipations(training_id)
+  ]);
 
   return (
     <div className="container mx-auto mt-6 p-4">
